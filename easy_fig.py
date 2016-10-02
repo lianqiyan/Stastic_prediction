5bc2262c3981b30013b0d5fbf43fddc892da428f
lianqiyan <lianqiyan@outlook.com>
2016-10-02 21:56:55 +0800

    Create easy_fig.py

---
+++ easy_fig.py
@@ -0,0 +1,35 @@
+import pandas as pd
+from rpy2.robjects.lib import ggplot2
+import numpy as np
+import matplotlib.pyplot as plt
+from rpy2.robjects import pandas2ri
+pandas2ri.activate()
+
+x = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+y = np.random.normal(10, 4, 10)
+color = np.random.normal(20, 4, 10)
+df = pd.DataFrame({'x': x,
+                   'y': y,
+                   'color': color})
+rdf = pandas2ri.py2ri(df)
+# print(type(rdf))
+plt.figure(1)
+pp = ggplot2.ggplot(rdf) + \
+     ggplot2.aes_string(x='x', y='y', fill='color') + \
+     ggplot2.geom_bar(stat="identity")
+pp.plot()
+plt.figure(2)
+gp = ggplot2.ggplot(rdf)
+pp = gp + \
+     ggplot2.aes_string(x='x', y='y', group=1, col='factor(color)') + \
+     ggplot2.geom_point(colour="grey50", size=4) + \
+     ggplot2.geom_point(size=2.5) + \
+     ggplot2.geom_line()
+pp.plot()
+plt.figure(3)
+vv = ggplot2.ggplot(rdf) + \
+     ggplot2.aes_string(x='factor(y)', fill='factor(color)') +\
+     ggplot2.geom_bar(width=1) +\
+     ggplot2.coord_polar(theta='x')
+vv.plot()
+
