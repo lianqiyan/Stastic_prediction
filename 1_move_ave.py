72ff026b3d11646fd588f35c7f70fd03822f28ac
Qiyan Lian <lianqiyan@outlook.com>
2016-10-22 19:33:14 +0800

    Update 1_move_ave.py

--- 1_move_ave.py
+++ 1_move_ave.py
@@ -11,8 +11,10 @@ def simple_move_aveage(data, n):
         else :
             k += 1
         pre[k] = sum(data[i:i+n])/n
-    print(pre)
+    return pre
 
 data = [46, 50, 59, 57, 55, 64, 55, 61, 45, 49, 46]
-simple_move_aveage(data, 1)
-simple_move_aveage(data, 3)
+p1 = simple_move_aveage(data, 1)
+print(p1)
+p2 = simple_move_aveage(data, 3)
+print(p2)
