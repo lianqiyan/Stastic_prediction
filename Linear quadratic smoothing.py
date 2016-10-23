0244a937039012001188222a2d5355ad0bb644a2
Qiyan Lian <lianqiyan@outlook.com>
2016-10-23 12:36:10 +0800

    Create Linear quadratic smoothing.py

---
+++ Linear quadratic smoothing.py
@@ -0,0 +1,55 @@
+import numpy as np
+
+
+def simple_move_aveage(data, n):
+    if data[0] == 0.0:
+        for i in range(0, len(data)):
+            if data[i] != 0:
+                mark = i
+                break
+        temp = data[mark:]
+    else:
+        temp = data
+    pre = np.tile([0.0], len(temp))
+    for i in range(0, len(temp) - n + 1):
+        if i is 0:
+            k = i + n - 1
+        else :
+            k += 1
+        pre[k] = sum(temp[i:i+n])/n
+    if data[0] == 0.0:
+        pred = list(data[0:mark]) + list(pre)
+        pred = np.around(pred, decimals=3)
+        return pred
+    else:
+        pre = np.around(pre, decimals=3)
+        return pre
+
+
+def cal_a_b(dat1, dat2, N):
+    at = np.tile([0.0], len(dat1))
+    bt = np.tile([0.0], len(dat1))
+    for i in range(0, len(dat1)):
+        if dat1[i] != 0 and dat2[i] != 0:
+            at[i] = 2 * dat1[i] - dat2[i]
+            bt[i] = (2/(N - 1)) * (dat1[i] - dat2[i])
+    return at, bt
+
+
+def predict(a, b, T):
+    Ft = np.tile([0.0], len(a)+1)
+    for i in range(0, len(a)):
+        if a[i] != 0.0:
+            Ft[i+1] = a[i] + b[i] * T
+    return Ft
+
+
+pure = [125, 135, 195, 197, 186, 175, 155, 190, 220,219, 226, 198, 260, 245]
+S1 = simple_move_aveage(pure, 3)
+print(S1)
+S2 = simple_move_aveage(S1, 3)
+print(S2)
+at, bt = cal_a_b(S1, S2, 3)
+print(at, '\n', bt)
+f = predict(at, bt, 1)
+print(f)
