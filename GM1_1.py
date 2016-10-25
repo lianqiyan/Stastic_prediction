b4d57b530e2ddae9e1ae5a94c708670962e1804c
Qiyan Lian <lianqiyan@outlook.com>
2016-10-25 09:16:56 +0800

    Update GM1_1.py

--- GM1_1.py
+++ GM1_1.py
@@ -1,20 +1,16 @@
 import numpy as np
+import numpy.matlib
 
 
 def cal_coef(mat):
-    x1 = [None] * len(mat)
-    for i in range(0, len(mat)):
-        x1[i] = sum(mat[0:i+1])
-    x1 = np.around(x1, decimals=3)
-    csum = np.matrix(np.tile(None, [len(mat)-1, 2]))
+    x1 = np.cumsum(mat)
+    B = np.matlib.zeros([len(mat)-1, 2])
     Yn = np.matrix(mat[1:]).transpose()
-    for i in range(0, csum.shape[0]):
-        csum[i, 0] = -1/2 * (x1[i] + x1[i+1])
-        csum[i, 1] = 1
-    B = np.asmatrix(csum)
-    print(B, Yn)
-    a = np.linalg.inv(B.transpose() * B)# * B.transpose() * Yn
-    print(csum, type(csum), a)
+    for i in range(0, B.shape[0]):
+        B[i, 0] = -1/2 * (x1[i] + x1[i+1])
+        B[i, 1] = 1
+    a = np.linalg.inv(B.transpose() * B) * B.transpose() * Yn
+    return a
 
 data = [26.7, 31.5, 32.8, 34.1, 35.8, 37.5]
 cal_coef(data)
