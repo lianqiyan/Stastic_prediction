477ae9dfb428b2c15611ebbdfde4928f1af941ce
Qiyan Lian <lianqiyan@outlook.com>
2016-10-25 22:46:43 +0800

    Update GM1_1.py

--- GM1_1.py
+++ GM1_1.py
@@ -2,7 +2,7 @@ import numpy as np
 import numpy.matlib
 
 
-def cal_coef(mat):
+def GM(mat):
     x1 = np.cumsum(mat)
     B = np.matlib.zeros([len(mat)-1, 2])
     Yn = np.matrix(mat[1:]).transpose()
@@ -10,7 +10,14 @@ def cal_coef(mat):
         B[i, 0] = -1/2 * (x1[i] + x1[i+1])
         B[i, 1] = 1
     a = np.linalg.inv(B.transpose() * B) * B.transpose() * Yn
-    return a
+    n1 = data[0] - a[1]/a[0]
+    n2 = a[1]/a[0]
+    print(n1, n2)
+    predict = lambda x: n1 * np.exp(-a[0]*x) + n2
+    p = predict(np. linspace(0, 5, 6))
+    print(p)
+    print(np.diff(p))
+    # return a
 
 data = [26.7, 31.5, 32.8, 34.1, 35.8, 37.5]
-cal_coef(data)
+GM(data)
