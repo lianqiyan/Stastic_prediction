08fac21f0d5413663a6f70c67d7c15748de82b17
Qiyan Lian <lianqiyan@outlook.com>
2016-10-26 20:58:39 +0800

    Update GM1_1.py

--- GM1_1.py
+++ GM1_1.py
@@ -12,12 +12,45 @@ def GM(mat):
     a = np.linalg.inv(B.transpose() * B) * B.transpose() * Yn
     n1 = data[0] - a[1]/a[0]
     n2 = a[1]/a[0]
-    print(n1, n2)
     predict = lambda x: n1 * np.exp(-a[0]*x) + n2
-    p = predict(np. linspace(0, 5, 6))
-    print(p)
-    print(np.diff(p))
-    # return a
+    p = (predict(np. linspace(0, mat.shape[0]-1, mat.shape[0]))).A1
+    p_data = np.hstack((np.array(p[0]), np.diff(p)))
+    p_data = np.around(p_data, 6)
+    return p_data, predict
 
-data = [26.7, 31.5, 32.8, 34.1, 35.8, 37.5]
-GM(data)
+
+def get_std(dat):
+    m = np.around(np.mean(dat), 5)
+    s = np.sqrt(np.sum(np.power(dat - m, 2))/(dat.shape[0]-1))
+    return s
+
+
+def cal_error(pdat, dat, pho):
+    abs_er = np.abs(pdat - dat)
+    rel_er = abs_er/dat
+    if np.max(rel_er) < 0.05:
+        print("The accuracy of the model is high!")
+    else:
+        print("The accuracy of the model is just fine")
+    amin = np.min(abs_er)
+    amax = np.max(abs_er)
+    p = lambda x: (amin + pho*amax)/(x + pho*amax)
+    eta = p(abs_er)  # correlation coefficient
+    r = np.sum(eta) / eta.shape[0]  # correlation degree
+    p_std = get_std(dat)  # std of raw data
+    abs_std = get_std(abs_er)  # std of absolute error
+    c = abs_std/p_std
+    S0 = 0.6745*p_std
+    ei = np.abs(abs_er - np.mean(abs_er))
+    if np.sum(ei > np.tile(S0, len(ei))) == 0 and c < 0.35:
+        print("The model was tested well")
+    else:
+        print("Oh...It seems like not good enough")
+    print('C:', c, '\n', 'S0:', S0, '\n', 'ei:', ei)
+
+
+data = np.array([26.7, 31.5, 32.8, 34.1, 35.8, 37.5])
+pre, pr = GM(data)
+cal_error(pre, data, 0.5)
+N8 = pr(7) - pr(6)
+print('The prediction of No.8 is ', N8)
